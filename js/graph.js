6ee9694d704f6e2b2327c0d5110fd2f47d04f676
Keenan Karich <keenan@woollyally.com>
2017-02-02 01:46:14 +0000

    Updated 'js/graph.js' via CloudCannon

--- js/graph.js
+++ js/graph.js
@@ -9,19 +9,19 @@
 				var ctx = document.getElementById("barChartCanvas").getContext("2d");
 				// new Chart(ctx).Bar(barChartData,globalGraphSettings);
 				var data = {
-				labels : ["2009","2010","2011","2012"],
+				labels : ["2009","2010","2011","2012", "2016"],
                 datasets: [
     					{
                             label: "Total Births",
     						backgroundColor : "rgba(125, 168, 137,0.5)",
     						borderColor : "rgba(125, 168, 137,1)",
-    						data : [35,91,114,131]
+    						data : [35,91,114,131, 193]
     					},
     					{
                             label: "Natural Births",
     						backgroundColor : "rgba(220,220,220,0.5)",
     						borderColor : "rgba(220,220,220,1)",
-    						data : [34,86,109,120]
+    						data : [34,86,109,120, 183]
     					}
     				],
                 };
